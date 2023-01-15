703630f431e4b6ae4dda51516e280ae177b0897b
Megha <98228827+meghalakhwan@users.noreply.github.com>
2023-01-15 23:46:46 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,8 @@
+public class Cube {
+    public static void main(String[] args) {
+        int x =4;
+        int cube = x*x*x;
+        System.out.println("The side is : "+ x);
+        System.out.println("The cube is : "+ cube);
+    }
+}
---
+++ TwoDiffString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class TwoDiffString
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first_name = sc.next();
+        String last_name = sc.next();
+        System.out.println(first_name + " " + last_name);
+
+    }
+}
